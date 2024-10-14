a3a2ec0499fee82fd582a0e1371a891d7ba9aba9
ggron <ggron@172.16.109.238>
2024-10-14 17:48:36 +0900

    feat: 유효성 검사 추가
    -@Validated 이용하여 유효성 검사 개발 완료

--- web/spring/springBootMVCShopping/src/main/java/springbootmvcshopping/command/MemberCommand.java
+++ web/spring/springBootMVCShopping/src/main/java/springbootmvcshopping/command/MemberCommand.java
@@ -4,23 +4,40 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class MemberCommand {
+	// 자료형이 String : @NotEmpty, @NotBlank 사용가능
+	// String이 아닌 자료형은 @NotNull이다
 	String memberNum;
+	@NotEmpty(message = "아이디를 입력해주세요")
 	String memberId;
+	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+			message = "영문자와 숫자 그리고 특수문자가 포함된 8글자 이상")
 	String memberPw;
+	@NotBlank(message = "비밀번호 확인을 입력해주세요") 
 	String memberPwCon;
+	@NotBlank(message = "이름을 입력해주세요") 
 	String memberName;
+	@NotBlank(message = "주소를 입력해주세요") 
 	String memberAddr;
 	String memberAddrDetail;
 	String memberPost;
+	@NotBlank(message = "전화번호를 입력해주세요")
+	@Size(min = 11, max = 23) // 길이 검사
 	String memberPhone1;
+	@Size(min = 11, max = 23)
 	String memberPhone2;
 	String gender;
+	@NotBlank(message = "이메일을 입력해주세요")
 	String memberEmail;
-	
+	@NotNull(message = "생년월일을 입력해 주세요")
 	@DateTimeFormat(pattern = "yyyy-mm-dd") // == SimpleDateFormet
 	Date memberBirth;
 	
--- web/spring/springBootMVCShopping/src/main/java/springbootmvcshopping/controller/MemberController.java
+++ web/spring/springBootMVCShopping/src/main/java/springbootmvcshopping/controller/MemberController.java
@@ -3,6 +3,8 @@ package springbootmvcshopping.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,15 +30,24 @@ public class MemberController {
 	
 	@GetMapping("memberWrite")
 	public String write(Model model) {
-		autoNumService.execute(model, "mem_", "member_num", 4, "members");
+		String autoNum = autoNumService.execute("mem_", "member_num", 5, "members");
+		MemberCommand memberCommand = new MemberCommand();
+		memberCommand.setMemberNum(autoNum);
+		model.addAttribute("memberCommand", memberCommand);
 		return "thymeleaf/member/memberForm";
 		//return "member/memberForm";
 	}
 	
 	@PostMapping("memberRegist")
-	public String wirte(MemberCommand memberCommand, Model model) {
+	public String wirte(@Validated MemberCommand memberCommand, BindingResult result
+			/*, Model model*/) { // BindingResult는 무조건 command뒤에
+		if (result.hasErrors()) {
+			return "thymeleaf/member/memberForm";
+		}
 		if (!memberCommand.isMemberPwEqualmemberPwCon()) {
-			model.addAttribute("errPw","비밀번호가 일치하지 않습니다.");
+			// model.addAttribute("errPw","비밀번호가 일치하지 않습니다.");
+			result.rejectValue("memberPwCon", "memberCommand.memberPwCon", 
+					"비밀번호가 일치하지 않습니다.");
 			return "thymeleaf/member/memberForm";
 		}
 		memberWriteService.execute(memberCommand);
