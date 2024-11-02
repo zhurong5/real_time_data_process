bd54beb31174b6d7fe8e93aafe9082ee1f3a0c8d
ggron <ggron@zhurong>
2024-11-02 19:25:57 +0900

    중간정리

--- web/spring/springBootMVCShopping/src/main/java/springbootmvcshopping/SpringBootMvcShoppingApplication.java
+++ web/spring/springBootMVCShopping/src/main/java/springbootmvcshopping/SpringBootMvcShoppingApplication.java
@@ -1,12 +1,19 @@
 package springbootmvcshopping;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import springbootmvcshopping.command.LoginCommand;
+import springbootmvcshopping.service.goods.MainGoodsListService;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // DB없으면 안찾음 있으면 찾음
 @SpringBootApplication
@@ -16,10 +23,22 @@ public class SpringBootMvcShoppingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcShoppingApplication.class, args);
 	}
-	
-	@RequestMapping("/")
-	public String index(LoginCommand loginCommand) {
+	@Autowired
+	MainGoodsListService mainGoodsListService;
+	@GetMapping("/")
+	public String index(LoginCommand logincommand
+			//,@RequestParam(value="page", required = false , defaultValue = "1") Integer page
+			//,Model model
+			) {
+		//mainGoodsListService.execute(page, model);
 		return "thymeleaf/index";
+	}
+	@PostMapping("/")
+	public ModelAndView index(int page, Model model) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("jsonView");
+		mainGoodsListService.execute(page, model);
+		return mav;
 	}
 
 }
